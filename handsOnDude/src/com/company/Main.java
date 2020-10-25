85145a9354164401b4aff16ef6eb639d458d5a28
Alper <akif414181@gmail.com>
2020-10-25 14:42:16 +0300

    Basics of maze

--- handsOnDude/src/com/company/Main.java
+++ handsOnDude/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       int[][] data = {
+               {4, 6 ,10, 3},
+               {12, 123, 2, 1},
+               {34, 99, 12, 46}
+       };
+
+        System.out.println((data[2][1]));
+
+
     }
 }
